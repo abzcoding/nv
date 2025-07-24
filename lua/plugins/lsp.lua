2caf664741db5f614f103d6d97a78255224027dd
abzcoding <abzcoding@gmail.com>
2025-07-24 09:47:47 +0330

    actually fixing clangd
    Signed-off-by: abzcoding <abzcoding@gmail.com>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -44,6 +44,91 @@ return {
       enabled = false,
     }
     -- opts.servers = opts.servers or {}
+    opts.servers.yamlls = {
+      cmd = { "yaml-language-server", "--stdio" },
+      filetypes = { "yaml", "gha", "dependabot", "yaml", "yaml.docker-compose", "yaml.gitlab" },
+      root_markers = { ".git" },
+      settings = {
+        redhat = { telemetry = { enabled = false } },
+        yaml = {
+          schemaStore = {
+            enable = false,
+            url = "",
+          },
+          schemas = require("schemastore").yaml.schemas(),
+          validate = true,
+          format = {
+            enable = false, -- delegate to conform.nvim
+          },
+        },
+      },
+    }
+    opts.servers.dockerls = {
+      cmd = { "docker-langserver", "--stdio" },
+      filetypes = { "dockerfile" },
+      root_markers = { "Dockerfile" },
+      settings = {
+        docker = {},
+      },
+    }
+    opts.servers.clangd = {
+      filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+      keys = {
+        { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+      },
+      root_dir = function(fname)
+        return require("lspconfig.util").root_pattern(
+          ".clang-format",
+          ".clang-tidy",
+          ".clangd",
+          ".git",
+          "Makefile",
+          "build.ninja",
+          "compile_commands.json",
+          "compile_flags.txt",
+          "config.h.in",
+          "configure.ac",
+          "configure.in",
+          "meson.build",
+          "meson_options.txt"
+        )(fname)
+      end,
+      capabilities = {
+        offsetEncoding = { "utf-16" },
+        textDocument = {
+          completion = {
+            editsNearCursor = true
+          }
+        }
+      },
+      cmd = {
+        "clangd",
+        "--all-scopes-completion",
+        "--background-index",
+        "--clang-tidy",
+        "--completion-style=detailed",
+        "--enable-config",
+        "--fallback-style=llvm",
+        "--function-arg-placeholders=1",
+        "--header-insertion-decorators",
+        "--header-insertion=iwyu",
+        "--log=error",
+        "--offset-encoding=utf-16",
+        "--pch-storage=memory",
+        "--ranking-model=heuristics",
+        "-j=12",
+      },
+      init_options = {
+        usePlaceholders = true,
+        completeUnimported = true,
+        clangdFileStatus = true,
+      },
+    }
+    opts.setup.clangd = function(_, _)
+      require("lspconfig").clangd.setup(opts.servers.clangd)
+      require("clangd_extensions").setup()
+      return true
+    end
     local pylance_bundle = vim.fn.expand("~/.pylance/extension/dist/server.bundle.js")
     if vim.uv.fs_stat(pylance_bundle) then
       local root_files = {
@@ -58,78 +143,6 @@ return {
         local ep = vim.fn.exepath(expr)
         return ep ~= "" and ep or nil
       end
-      opts.servers.yamlls = {
-        cmd = { "yaml-language-server", "--stdio" },
-        filetypes = { "yaml", "gha", "dependabot", "yaml", "yaml.docker-compose", "yaml.gitlab" },
-        root_markers = { ".git" },
-        settings = {
-          redhat = { telemetry = { enabled = false } },
-          yaml = {
-            schemaStore = {
-              enable = false,
-              url = "",
-            },
-            schemas = require("schemastore").yaml.schemas(),
-            validate = true,
-            format = {
-              enable = false, -- delegate to conform.nvim
-            },
-          },
-        },
-      }
-      opts.servers.dockerls = {
-        cmd = { "docker-langserver", "--stdio" },
-        filetypes = { "dockerfile" },
-        root_markers = { "Dockerfile" },
-        settings = {
-          docker = {},
-        },
-      }
-      opts.servers.clangd = {
-        filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
-        keys = {
-          { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
-        },
-        root_dir = function(fname)
-          return require("lspconfig.util").root_pattern(
-            "main.c",
-            "Makefile",
-            "configure.ac",
-            "configure.in",
-            "config.h.in",
-            "meson.build",
-            "meson_options.txt",
-            "build.ninja"
-          )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
-            fname
-          ) or vim.fs.dirname(vim.fs.find('.git') { path = fname, upward = true })[1]
-        end,
-        capabilities = {
-          offsetEncoding = { "utf-16" },
-        },
-        cmd = {
-          "clangd",
-          "--all-scopes-completion",
-          "--background-index",
-          "--clang-tidy",
-          "--completion-style=detailed",
-          "--enable-config",
-          "--fallback-style=llvm",
-          "--function-arg-placeholders",
-          "--header-insertion-decorators",
-          "--header-insertion=iwyu",
-          "--log=error",
-          "--offset-encoding=utf-16",
-          "--pch-storage=memory",
-          "--ranking-model=heuristics",
-          "-j=12",
-        },
-        init_options = {
-          usePlaceholders = true,
-          completeUnimported = true,
-          clangdFileStatus = true,
-        },
-      }
       local configs = require("lspconfig.configs")
       configs.pylance = {
         default_config = {
@@ -163,7 +176,18 @@ return {
           },
         },
       }
-      require("lspconfig").pylance.setup({})
+      opts.setup.pylance = function(_, opts)
+        require("lspconfig").pylance.setup({
+          cmd = {
+            "node",
+            pylance_bundle,
+            "--stdio",
+          },
+          filetypes = { "python" },
+        })
+        return true
+      end
     end
+    return opts
   end,
 }
