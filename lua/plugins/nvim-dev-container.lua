190894c5017d79fc6cc4ed63b0156897b05f6cd7
abzcoding <abzcoding@gmail.com>
2026-05-14 09:57:10 +0330

    use alternative nvim-dev-container
    Signed-off-by: abzcoding <abzcoding@gmail.com>

--- lua/plugins/nvim-dev-container.lua
+++ lua/plugins/nvim-dev-container.lua
@@ -1,22 +1,4 @@
 return {
   event = "VeryLazy",
-  "esensar/nvim-dev-container",
-  config = function()
-    require("devcontainer").setup({
-      attach_mounts = {
-        neovim_config = {
-          enabled = true,
-          options = { "readonly" }
-        },
-        neovim_data = {
-          enabled = true,
-          options = {}
-        },
-        neovim_state = {
-          enabled = true,
-          options = {}
-        },
-      },
-    })
-  end
+  "debdutdeb/devcontainer.nvim",
 }
