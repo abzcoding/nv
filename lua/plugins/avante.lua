2ca11fae2acabf6395ae4401bb2a85b3965be50e
abzcoding <abzcoding@gmail.com>
2026-07-05 04:31:28 +0330

    fix the fix
    Signed-off-by: abzcoding <abzcoding@gmail.com>

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -2,7 +2,7 @@ local function avante_prompt(name)
   return require("config.prompts").avante[name]
 end
 
-local is_offline = require("config.utils").is_online()
+local is_online = require("config.utils").is_online()
 
 local function create_avante_call(prompt, use_context)
   if use_context then
