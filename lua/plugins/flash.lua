45e5e93ceba64cee97013acc81ce6a3dffd6760b
abzcoding <abzcoding@gmail.com>
2026-07-19 14:38:07 +0330

    fix flash + update deps
    Signed-off-by: abzcoding <abzcoding@gmail.com>

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,5 +1,7 @@
 return {
   "folke/flash.nvim",
+  -- TODO: Remove when https://github.com/folke/flash.nvim/pull/492 is merged.
+  commit = "70da6362c68772a6d2e316b691b701bf6b53936f",
   opts = function(_, opts)
     opts = opts or {}
     opts.modes = {
